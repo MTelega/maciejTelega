43882d440c16489485136121f1e9035136820cc2
Maciej Telega <m.telega@outlook.com>
2022-01-18 21:04:36 +0000

    'country borders'

--- project1/js/script.js
+++ project1/js/script.js
@@ -4,3 +4,35 @@ L.tileLayer.provider('Jawg.Streets', {
     variant: 'jawg-terrain',
     accessToken: '6oG0Hxo13keGOII2LHv78deYiRkASGVGTynxQ9fiKZRXiHRR6Xo9dWQXy7X1G0T8'
 }).addTo(map);
+
+map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+  var radius = e.accuracy;
+
+  L.marker(e.latlng).addTo(map)
+      .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+  L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+$('#select').click(function() {
+  $.ajax({
+      url: "../php/countrySelector.php",
+      type: 'GET',
+      dataType: 'json',
+      success: function(result) {
+          console.log(JSON.stringify(result));
+          if (result.status.name == "ok") {
+            $('#select').append(`<option value="${data.features.properties.iso_a2}">${data.features.properties.name}</option>`);
+          }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          // your error code
+          console.log(errorThrown);
+          console.log(textStatus);      
+      }
+  });
+});
---
+++ project1/php/countrySelector.php
@@ -0,0 +1,13 @@
+<?php
+$url = '../countryBorders.geo.json';
+$data = file_get_contents($url);
+$decode = json_decode($data, true);
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+$output['data'] = $decode;
+
+echo json_encode($output);
+?>
