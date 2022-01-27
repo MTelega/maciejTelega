01cac978151a89a315089262e5e4ba36d23dd6e0
Maciej Telega <m.telega@outlook.com>
2022-01-27 15:56:01 +0000

    'changes applied'

--- project1/js/script.js
+++ project1/js/script.js
@@ -24,12 +24,87 @@ $(function() {
   });
 });
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var mapOptions = {
+  center: [51.505, -0.09],
+  zoom: 10,
+  zoomControl: false
+}
+
+var map = L.map('map', mapOptions);
+
+// zoom control options
+var zoomOptions = {
+  zoomInText: '+',
+  zoomOutText: '-',
+  position: 'bottomright'
+};
+// Creating zoom control
+var zoom = L.control.zoom(zoomOptions);
+zoom.addTo(map);
 
 L.tileLayer.provider('Jawg.Streets', {
     variant: 'jawg-terrain',
     accessToken: '6oG0Hxo13keGOII2LHv78deYiRkASGVGTynxQ9fiKZRXiHRR6Xo9dWQXy7X1G0T8'
 }).addTo(map);
+
+L.Control.textbox = L.Control.extend({
+  onAdd: function(map) {
+    
+  var text = L.DomUtil.create('div');
+  text.id = "info_text";
+  text.innerHTML = "<h1 id='demographic'></h1>" +
+                    "<img src='' alt=''><br>" +
+                    "<p id='name'></p>" +
+                    "<p id='languages'></p>" +
+                    "<p id='population'></p>" +
+                    "<p id='currency'></p><br>" +
+                    "<h1 id='geographic'></h1>" +
+                    "<p id='capitol'></p>" +
+                    "<p id='continent'></p>" +
+                    "<p id='area'></p>" +
+                    "<p id='lat'></p>" +
+                    "<p id='lon'></p><br>" +
+                    "<h1 id='forecast'></h1>" +
+                    "<p id='weather'></p>" +
+                    "<div id='w-fidget'></div>" +
+                    "<p id='air'></p><br>" +
+                    "<h1 id='exchange'></h1>" +
+                    "<p id='rate'></p><br>" +
+                    "<h1 id='news'></h1>" +
+                    "<h1 id='wiki-links'></h1>";
+  return text;
+  },
+
+  onRemove: function(map) {
+    // Nothing to do here
+  }
+});
+L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+L.control.textbox({ position: 'topleft' }).addTo(map);
+
+$('#select').click(function() {
+  $.ajax({
+      url: "",
+      type: 'POST',
+      dataType: 'json',
+      data: {
+          Country: $('#select').val(),
+
+      },
+      success: function(result) {
+          console.log(JSON.stringify(result));
+          if (result.status.name == "ok") {
+
+           
+
+          }
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+          // your error code
+          console.log(errorThrown, textStatus);
+      }
+  });
+});
 
 
 
