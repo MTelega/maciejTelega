628a164be26ec7fa96abfced348d32e7b876623f
Maciej Telega <m.telega@outlook.com>
2022-04-27 04:00:35 +0100

    'data display if email not provided'

--- companyDirectory/js/main.js
+++ companyDirectory/js/main.js
@@ -55,12 +55,17 @@ $( document ).ready(function() {
         $('.depart').html(data[0].department);
         $('.loc').html(data[0].location);
         if(data[0].jobTitle != ''){
-            $('.jTitleP').removeClass('d-none');
+            $('.jTitleP, .jobT').removeClass('d-none');
             $('.jobT').html(data[0].jobTitle);
         } else {
-            $('.jTitleP').addClass('d-none');
+            $('.jTitleP, .jobT').addClass('d-none');
         }
-        $('.mail').html(data[0].email);
+        if(data[0].email != ''){
+            $('.mailP, .mail').removeClass('d-none');
+            $('.mail').html(data[0].email);
+        } else {
+            $('.mailP, .mail').addClass('d-none');
+        }
         $('#updatePersonnelButton').val(data[0].id)
         $('#updatePersonnelFirstName').val(data[0].firstName);
         $('#updatePersonnelLastName').val(data[0].lastName);
@@ -270,7 +275,12 @@ function locationGetAll() {
                 } else {
                     $('.jTitleP').addClass('d-none');
                 }
-                $('.mail').html(result.data[0].email);
+                if(data[0].email != ''){
+                    $('.mailP, .mail').removeClass('d-none');
+                    $('.mail').html(data[0].email);
+                } else {
+                    $('.mailP, .mail').addClass('d-none');
+                }
                 $('#updatePersonnelButton').val(result.data[0].id)
                 $('#updatePersonnelFirstName').val(result.data[0].firstName);
                 $('#updatePersonnelLastName').val(result.data[0].lastName);
